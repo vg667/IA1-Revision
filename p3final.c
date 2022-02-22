e8eb341febd66765256c0c6127a029260ab09924
Vinaya001 <93110662+vg667@users.noreply.github.com>
2022-02-22 04:11:13 +0000

    nice

--- p3final.c
+++ p3final.c
@@ -1,2 +1,32 @@
 #include <stdio.h>
-int main ()
+int input ()
+{
+  int a;
+  printf("enter the numbers:\n");
+    scanf("%d",&a);
+      return a;
+}
+int sum_n(int n)
+{
+  int sum=0;
+  for(int i=0;i<n;i++)
+    sum+=i;
+  return sum;
+}
+void output(int n,int sum)
+{
+  for(int i=1;i<n;i++)
+    {
+      printf("%d+",i);
+    }
+  printf("%d is %d",n,sum);
+}
+int main()
+{
+  int n,sum;
+  n=input();
+  sum=sum_n(n);
+  output(n,sum);
+  return 0;
+}
+
